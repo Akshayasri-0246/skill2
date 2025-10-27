54dc86e0fdced78f6324de0d3ab2e054cbcba86b
Akshayasri-0246 <230701024@rajalakshmi.edu.in>
2025-10-27 13:00:04 +0530

    Add blog routes for creating and retrieving blogs

---
+++ blogRoutes.js
@@ -0,0 +1,9 @@
+// routes/blogRoutes.js
+const express = require('express');
+const router = express.Router();
+const blogController = require('../controllers/blogController');
+
+router.post('/blogs', blogController.createBlog);
+router.get('/blogs', blogController.getBlogs);
+
+module.exports = router;

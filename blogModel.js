7c0f231e2556172be4afc079eb89c861c6c578ae
Akshayasri-0246 <230701024@rajalakshmi.edu.in>
2025-10-27 12:58:42 +0530

    Create blogModel.js with Mongoose schema

---
+++ blogModel.js
@@ -0,0 +1,22 @@
+//blogModel.js
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required']
+    },
+    content: {
+      type: String,
+      required: [true, 'Content is required']
+    },
+    author: {
+      type: String,
+      default: 'Anonymous'
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Blog', blogSchema);
